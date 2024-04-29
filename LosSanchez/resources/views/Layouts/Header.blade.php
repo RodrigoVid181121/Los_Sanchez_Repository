e95440b05e3949decd67fa699111b2e51ad6286d
EsmeRod <89407510+EsmeRod@users.noreply.github.com>
2024-04-28 18:07:02 -0600

    home prueba

---
+++ LosSanchez/resources/views/Layouts/Header.blade.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-nav box-shadow mb-3">
+            <div class="box-container">           
+                <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+                    <ul class="navbar-nav flex-grow-1 menu">
+                            <li class="nav-item">
+                                <a class="nav-link"><img src="/LIS-P/img/Logo_Sanchez.png"style="height:4vw;"/></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Sobre Nosotros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Menu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link">Reservaciones</a>
+                            </li>
+                            <li class="nav-item">
+                                <div class="action">
+                                <div onclick="sesionToggle();">
+                                <a class="nav-link" style="color:blue;">Inicio Sesion</a>
+                                </div>
+                                <div class="sesion">
+                                    <img src="/LIS-P/img/Icon-User.png" style="padding-left:2vw;">
+                                    <h3>USUARIO</h3>
+                                    <ul>
+                                    <li>
+                                    <button href="#" class="btn btn-primary bt-user">Cerrar Sesión</button>
+                                    </li>
+                                    <li>
+                                    <button href="#" class="btn btn-primary bt-user">Usuarios</button>
+                                    </li>
+                                    <li>
+                                    <button href="#" class="btn btn-primary bt-user">Clientes</button>
+                                    </li>          
+                                    </ul>
+                                </div>
+                                </div>  
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
---
+++ LosSanchez/resources/views/Vistas/Home.blade.php
@@ -0,0 +1,15 @@
+@extends('Layouts.Layout')
+@section('content')
+
+<div class="row">
+    <div class="container col-sm F-col">
+        <div class="box-cont">
+            <h2 class="enfasis">Delicioso</h2> 
+            <h3>No te quedes sin probar!</h3>
+            <p>El Sazón de Los Sanchez te dejará esperando por más ...</p>
+        </div>
+    </div>
+        <div class="col-sm" style="top: 0; margin-top: 0;">
+            <img src="/LIS-P/img/Food.png" class="img-fo" >
+    </div>                                                 
+</div>
