17c8013918cf5be683a7150c13427df089898665
RodrigoVid181121 <navasvides@gmail.com>
2024-05-12 18:44:28 -0600

    adding JS doc for Users 2

--- LosSanchez/resources/views/Vistas/Clientes/Registro.blade.php
+++ LosSanchez/resources/views/Vistas/Clientes/Registro.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="{{asset('css/main.css')}}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script href= "{{asset('js/Users.js')}}"></script>
     <title>Registro</title>
 
      <!--<script src="../../../js/bootstrap.min.js"></script>-->
